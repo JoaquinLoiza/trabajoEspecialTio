5cb4e8d9ea1dcdf769d1275d97fb7d1236905878
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2020-05-14 15:54:20 -0300

    Se realizo el codigo necesario para el ruteo

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'controllers/calculator.controller.php';
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+    if (empty($_GET['action'])) {
+        $_GET['action'] = 'home';
+    } 
+    $accion = $_GET['action']; 
+    $parametros = explode('/', $accion);
+    switch ($parametros[0]) {
+        case 'home':
+            $controller = new CalculatorController();
+            $controller->showCalculator();
+        break;
+        case 'nosotros':
+            $controller = new CalculatorController();
+            $controller->showCreators();
+        break;
+        default: 
+            echo "404 not found";
+        break;
+    }
