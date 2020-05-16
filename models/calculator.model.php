d7d08bda40e253c27fab110ff5dd9ebe5ff92082
Romina Dehesa <rominadehesa@gmail.com>
2020-05-16 01:41:52 -0300

    agregue el codigo necesario en el archivo calculator.model.php para conectarnos con la base de datos

--- models/calculator.model.php
+++ models/calculator.model.php
@@ -1,4 +1,27 @@
 <?php
 class CalculatorModel{
-    
+
+    private function createConection() {
+        $host = 'localhost'; 
+        $userName = 'root'; 
+        $password = '';
+        $database = 'db_creadores'; // nombre de la base de datos
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $userName, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        } catch (Exception $e) {
+            var_dump($e);
+        }
+        return $pdo;
+    }
+    public function getCreators(){
+        $db = $this->createConection();
+        
+           
+        $sentencia = $db->prepare("SELECT * FROM creadores"); 
+        $sentencia->execute();
+        $usuarios = $sentencia->fetchAll(PDO::FETCH_OBJ); 
+        
+        return $usuarios;
+    }
 }
