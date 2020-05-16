7a76729a21de06a5ea5ca5ee4b6f7bc75bb51e56
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-16 16:45:54 -0300

    Se creo el codigo necesario en el archivo calculator.view.php utilizando smarty.

--- views/calculator.view.php
+++ views/calculator.view.php
@@ -1,3 +1,17 @@
 <?php
- class CalculatorView{
- }
+    require_once ('libs/Smarty.class.php');
+    class CalculatorView{
+        private $smarty;
+        public function __construct(){
+            $this->smarty = new Smarty();
+            $this->smarty->assign('base_url', BASE_URL);
+            $this->smarty->assign('title','Calculadora');
+        }
+        public function viewCalculator(){
+            $this->smarty->display('calculadora.tpl');
+        }
+        public function viewCreator($creators){
+            $this->smarty->assign('creadores', $creators);
+            $this->smarty->display('quienesSomos.tpl');
+        }
+}
